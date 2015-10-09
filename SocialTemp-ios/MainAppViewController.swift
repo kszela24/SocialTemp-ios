3e8145d69cc938ad2ed4f52d75d86a4ca13936ab
Kyle Szela <kszela24@gmail.com>
2015-10-09 18:34:42 -0500

    Added a chart displaying last 12 hours.

--- SocialTemp-ios/MainAppViewController.swift
+++ SocialTemp-ios/MainAppViewController.swift
@@ -8,23 +8,68 @@
 
 import UIKit
 import Parse
+import Charts
 
-class MainAppViewController: UIViewController {
+class MainAppViewController: UIViewController, ChartViewDelegate {
     
     @IBOutlet var displayText: UILabel!
+    @IBOutlet var chartView: BarChartView!
+    var times:[String] = []
+    var percentages:[Int] = []
     
     override func viewDidLoad() {
         loadInfo()
         super.viewDidLoad()
+        
+        //NavBar
         self.navigationController?.navigationBar.barTintColor = UIColor(red: 67.0/255.0, green: 31.0/255.0, blue: 129.0/255.0, alpha: 1)
         self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
         self.title = "SocialTempNU"
+        
+        //Chart
+        chartView.delegate = self
+        chartView.descriptionText = "";
+        chartView.noDataTextDescription = "Data will be loaded soon."
+        chartView.drawBarShadowEnabled = false
+        chartView.drawValueAboveBarEnabled = true
+        chartView.maxVisibleValueCount = 60
+        chartView.pinchZoomEnabled = false
+        chartView.drawGridBackgroundEnabled = false
+        chartView.drawBordersEnabled = false
+        chartView.noDataText = ""
+        chartView.drawBordersEnabled = false
+        chartGetData()
+        
         // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    private func chartGetData() {
+        PFCloud.callFunctionInBackground("chartGetData", withParameters: nil) {
+            (response: AnyObject?, error: NSError?) -> Void in
+            let objects = response as! NSArray
+            let percentagesParse = objects[0] as! [Int]
+            let timesParse = objects[1] as! [String]
+            self.times += timesParse
+            self.percentages += percentagesParse
+            
+            var yVals: [BarChartDataEntry] = []
+            
+            for (index,percentage) in self.percentages.enumerate() {
+                yVals.append(BarChartDataEntry(value: Double(percentage), xIndex: index))
+            }
+            
+            let set1 = BarChartDataSet(yVals: yVals, label: nil)
+            set1.barSpace = 0.1
+            set1.colors = [UIColor(red: 67.0/255.0, green: 31.0/255.0, blue: 129.0/255.0, alpha: 1)]
+            let data = BarChartData(xVals: self.times, dataSet: set1)
+            self.chartView.data = data
+            self.view.reloadInputViews()
+        }
     }
     
     private func loadInfo() {
