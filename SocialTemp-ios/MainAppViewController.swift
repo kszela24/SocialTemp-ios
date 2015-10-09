4877775a606bfdccfa3ae8188dc4b65aba6f51a4
Kyle Szela <kszela24@gmail.com>
2015-10-09 16:34:04 -0500

    Added a Title Bar with NU colors and connected everything to Parse.

---
+++ SocialTemp-ios/MainAppViewController.swift
@@ -0,0 +1,37 @@
+//
+//  MainAppViewController.swift
+//  SocialTemp-ios
+//
+//  Created by Kyle on 10/9/15.
+//  Copyright © 2015 Kyle. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class MainAppViewController: UIViewController {
+    
+    @IBOutlet var displayText: UILabel!
+    
+    override func viewDidLoad() {
+        loadInfo()
+        super.viewDidLoad()
+        self.navigationController?.navigationBar.barTintColor = UIColor(red: 67.0/255.0, green: 31.0/255.0, blue: 129.0/255.0, alpha: 1)
+        self.navigationController?.navigationBar.titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
+        self.title = "SocialTempNU"
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    private func loadInfo() {
+        PFCloud.callFunctionInBackground("returnSentiment", withParameters: nil) {
+            (response: AnyObject?, error: NSError?) -> Void in
+            let objects = response as! NSArray
+            self.displayText.text = "Northwestern is " + String(objects[0]) + "% " + String(objects[1])
+        }
+    }
+}
