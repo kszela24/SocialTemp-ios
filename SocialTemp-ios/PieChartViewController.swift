b7bdd892580cde0a6863c73a8f8b15e898a14119
hspindell <henryspindell@gmail.com>
2015-10-24 15:56:53 -0500

    pie chart autolayout

--- SocialTemp-ios/PieChartViewController.swift
+++ SocialTemp-ios/PieChartViewController.swift
@@ -36,13 +36,11 @@ class PieChartViewController: UIViewController {
         
         var colors: [UIColor] = []
         
-        for i in 0..<dataPoints.count {
+        for _ in 0..<dataPoints.count {
             let red = Double(arc4random_uniform(256))
             let green = Double(arc4random_uniform(256))
             let blue = Double(arc4random_uniform(256))
-            
-            let color = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
-            colors.append(color)
+            colors.append(UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1))
         }
         
         pieChartDataSet.colors = colors
