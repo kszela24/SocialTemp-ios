7c79165615bdae4d1c6bb9fa2f53c51a18ec6b1d
hspindell <henryspindell@gmail.com>
2015-11-01 19:03:05 -0600

    added label on gradient for today's polarity

--- SocialTemp-ios/MainAppViewController.swift
+++ SocialTemp-ios/MainAppViewController.swift
@@ -12,11 +12,15 @@ import Charts
 
 class MainAppViewController: UIViewController, ChartViewDelegate, UIPickerViewDelegate, UIPickerViewDataSource {
     
+    @IBOutlet weak var otherDayLabel: UILabel!
+    @IBOutlet weak var todayLabel: UILabel!
     @IBOutlet weak var polarityGradientView: UIView!
     @IBOutlet weak var arrowImageView: UIImageView!
     @IBOutlet weak var trendDirectionBackground: UIView!
     @IBOutlet weak var polarityChangeLabel: UILabel!
     @IBOutlet weak var timePicker: UIPickerView!
+
+
 
     var percentages:[Int] = []
     var polarityCumulative:[Float] = []
@@ -28,6 +32,9 @@ class MainAppViewController: UIViewController, ChartViewDelegate, UIPickerViewDe
     
     var currentAveragePolarity:Float = 0
     var relativeSentiments = [Float]()
+    var dailyAveragePolarities = [Float]()
+    
+    var otherDayLabelYConstraint: NSLayoutConstraint?
     
     override func viewDidLoad() {
         loadInfo()
@@ -40,7 +47,7 @@ class MainAppViewController: UIViewController, ChartViewDelegate, UIPickerViewDe
 
         getSentimentTrends()
     }
-    
+
     override func viewDidAppear(animated: Bool) {
         let topColor = UIColor(red:0.27, green:0.62, blue:0.1, alpha:1).CGColor
         let bottomColor = UIColor(red:0.01, green:0.36, blue:0.69, alpha:1).CGColor
@@ -76,10 +83,12 @@ class MainAppViewController: UIViewController, ChartViewDelegate, UIPickerViewDe
             let currentAvgPolarity = objects[0] as! Float
             let relativeSentiments = objects[1] as! [Float]
             let times = objects[2] as! [String]
+            let dailyAveragePolarities = objects[3] as! [Float]
             
             self.currentAveragePolarity = currentAvgPolarity
             self.relativeSentiments = relativeSentiments
             self.pickerTimes = times
+            self.dailyAveragePolarities = dailyAveragePolarities
             
             self.timePicker.delegate = self
             self.timePicker.dataSource = self
@@ -87,7 +96,8 @@ class MainAppViewController: UIViewController, ChartViewDelegate, UIPickerViewDe
             self.pickerDateIndex = 0
             
             self.updateTrendUI()
-
+            
+            self.placeGradientMarkers()
         }
     }
     
@@ -105,7 +115,42 @@ class MainAppViewController: UIViewController, ChartViewDelegate, UIPickerViewDe
             self.arrowImageView.image = UIImage(named: "no_change")
         }
         
+        otherDayLabel.text = self.pickerTimes[pickerDateIndex]
         self.polarityChangeLabel.text = "\(abs(self.polarityChange))%"
+        
+    }
+    
+    func placeGradientMarkers() {
+        let gradientHeight = polarityGradientView.frame.height
+        
+        let todayLabelYConstraint = NSLayoutConstraint (item: polarityGradientView,
+            attribute: NSLayoutAttribute.Bottom,
+            relatedBy: NSLayoutRelation.Equal,
+            toItem: todayLabel,
+            attribute: NSLayoutAttribute.CenterY,
+            multiplier: 1,
+            constant: gradientHeight/2 + CGFloat(currentAveragePolarity) * gradientHeight/2)
+        view.addConstraint(todayLabelYConstraint)
+        print(currentAveragePolarity)
+        print(relativeSentiments)
+        print(dailyAveragePolarities)
+      
+        todayLabel.layer.zPosition = 1
+        otherDayLabel.layer.zPosition = 1
+        
+        otherDayLabelYConstraint = NSLayoutConstraint (item: polarityGradientView,
+            attribute: NSLayoutAttribute.Bottom,
+            relatedBy: NSLayoutRelation.Equal,
+            toItem: otherDayLabel,
+            attribute: NSLayoutAttribute.CenterY,
+            multiplier: 1,
+            constant: gradientHeight/2 + CGFloat(dailyAveragePolarities[pickerDateIndex]) * gradientHeight/2)
+        view.addConstraint(otherDayLabelYConstraint!)
+        
+        UILabel.animateWithDuration(1, animations: {
+            self.todayLabel.layoutIfNeeded()
+            self.otherDayLabel.layoutIfNeeded()
+        })
     }
     
     // PickerView delegate methods
@@ -124,6 +169,8 @@ class MainAppViewController: UIViewController, ChartViewDelegate, UIPickerViewDe
     func pickerView(pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         self.pickerDateIndex = row
         updateTrendUI()
+        
+        // TODO update other day marker position, update constraint
     }
     
 }
