bcdfd6a5f02f46983c1ce04e3af8cfd2716e6fc9
hspindell <henryspindell@gmail.com>
2015-11-23 22:44:22 -0600

    sample tweet added to UI, separated topics and emotions

---
+++ SocialTemp-ios/EmotionDataViewController.swift
@@ -0,0 +1,122 @@
+//
+//  PieChartViewController.swift
+//  SocialTemp-ios
+//
+//  Created by Kyle Szela on 10/23/15.
+//  Copyright © 2015 Kyle. All rights reserved.
+//
+
+import UIKit
+import Charts
+import Parse
+
+class EmotionDataViewController: UIViewController, ChartViewDelegate {
+    
+    
+    @IBOutlet weak var emotionPieChartView: PieChartView!
+
+    @IBOutlet weak var sampleTweetEmotionLabel: UILabel!
+    @IBOutlet weak var sampleTweetBodyLabel: UILabel!
+    
+    var emotionTally:[Double] = []
+    var emotions:[String] = []
+    var emotionTweetSamples = [String]()
+    var emotionColors = [UIColor]()
+
+    
+    private func getEmotions() {
+        PFCloud.callFunctionInBackground("returnEmotions", withParameters: nil) {
+            (response: AnyObject?, error: NSError?) -> Void in
+            let objects = response as! NSArray
+            self.emotionTally = objects[0] as! [Double]
+            self.emotions = objects[1] as! [String]
+            self.setEmotionChart(self.emotions, values: self.emotionTally)
+            self.emotionPieChartView.highlightValue(xIndex: 0, dataSetIndex: 0, callDelegate: true)
+        }
+    }
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = UIColor(red:0.24, green:0.24, blue:0.25, alpha:1)
+        self.title = "Emotions"
+        
+        self.navigationController?.navigationBar.tintColor = UIColor.whiteColor()
+        emotionPieChartView.delegate = self
+        
+        
+        let query = PFQuery(className:"DailyAverage")
+        query.orderByDescending("createdAt")
+        query.getFirstObjectInBackgroundWithBlock(){
+            (dailyAverage: PFObject?, error: NSError?) -> Void in
+            if let dailyAvg = dailyAverage, emotionSamples = dailyAvg["emotionSamples"] as? [String]
+                where error == nil {
+                    //                print(dailyAvg)
+                    //                print(dailyAvg["topicSamples"])
+                    self.emotionTweetSamples = emotionSamples
+            } else {
+                print(error)
+            }
+        }
+
+        getEmotions()
+    }
+
+    
+    func setEmotionChart(dataPoints: [String], values: [Double]) {
+
+        var dataEntries: [ChartDataEntry] = []
+
+        for i in 0..<dataPoints.count {
+            let dataEntry = ChartDataEntry(value: values[i], xIndex: i)
+            dataEntries.append(dataEntry)
+        }
+        let pieChartDataSet = PieChartDataSet(yVals: dataEntries, label: "")
+        let pieChartData = PieChartData(xVals: dataPoints, dataSet: pieChartDataSet)
+        emotionPieChartView.data = pieChartData
+        emotionPieChartView.centerText = "How is NU feeling?"
+        emotionPieChartView.centerTextLineBreakMode = NSLineBreakMode.ByWordWrapping
+        emotionPieChartView.descriptionText = "Emotions of NU Posts"
+        emotionPieChartView.usePercentValuesEnabled = true
+        emotionPieChartView.drawSliceTextEnabled = false
+        emotionPieChartView.descriptionTextColor = UIColor.whiteColor()
+        emotionPieChartView.legend.textColor = UIColor.whiteColor()
+        emotionPieChartView.backgroundColor = UIColor.darkGrayColor()
+
+        var colors: [UIColor] = []
+
+        for _ in 0..<dataPoints.count {
+            let red = Double(arc4random_uniform(256))
+            let green = Double(arc4random_uniform(256))
+            let blue = Double(arc4random_uniform(256))
+            colors.append(UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1))
+        }
+
+        pieChartDataSet.colors = colors
+        emotionColors = colors
+
+        emotionPieChartView.legend.textColor = UIColor.whiteColor()
+
+        emotionPieChartView.legendRenderer.computeLegend(pieChartData)
+        emotionPieChartView.legend.calculatedLabelBreakPoints = [false, false, false, false, true, false, false]
+
+
+        emotionPieChartView.legend.neededHeight = 20.0
+
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    func chartValueSelected(chartView: ChartViewBase, entry: ChartDataEntry, dataSetIndex: Int, highlight: ChartHighlight) {
+        // set topic label color to topicColors[entry.xIndex]
+
+        sampleTweetEmotionLabel.text = "Sample tweet displaying \(emotions[entry.xIndex])"
+        sampleTweetEmotionLabel.textColor = emotionColors[entry.xIndex]
+        
+        sampleTweetBodyLabel.text = "@anonymous: \(emotionTweetSamples[entry.xIndex])"
+    }
+    
+    
+}
--- SocialTemp-ios/TopicDataViewController.swift
+++ SocialTemp-ios/TopicDataViewController.swift
@@ -10,13 +10,22 @@ import UIKit
 import Charts
 import Parse
 
-class PieChartViewController: UIViewController {
+class TopicDataViewController: UIViewController, ChartViewDelegate {
+
+
+    @IBOutlet weak var sampleTweetTopicLabel: UILabel!
+    @IBOutlet weak var sampleTweetBodyLabel: UILabel!
+    
     @IBOutlet weak var topicPieChartView: PieChartView!
-    @IBOutlet weak var emotionPieChartView: PieChartView!
+
     var topicTally:[Double] = []
     var topics:[String] = []
-    var emotionTally:[Double] = []
-    var emotions:[String] = []
+    var topicTweetSamples = [String]()
+    
+
+    var topicColors = [UIColor]()
+    
+    
     private func getTopics() {
         PFCloud.callFunctionInBackground("returnTopics", withParameters: nil) {
             (response: AnyObject?, error: NSError?) -> Void in
@@ -24,27 +33,34 @@ class PieChartViewController: UIViewController {
             self.topicTally = objects[0] as! [Double]
             self.topics = objects[1] as! [String]
             self.setTopicChart(self.topics, values: self.topicTally)
+            self.topicPieChartView.highlightValue(xIndex: 0, dataSetIndex: 0, callDelegate: true)
         }
     }
-    private func getEmotions() {
-        PFCloud.callFunctionInBackground("returnEmotions", withParameters: nil) {
-            (response: AnyObject?, error: NSError?) -> Void in
-            let objects = response as! NSArray
-            self.emotionTally = objects[0] as! [Double]
-            self.emotions = objects[1] as! [String]
-            self.setEmotionChart(self.emotions, values: self.emotionTally)
-        }
-    }
-    
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(red:0.24, green:0.24, blue:0.25, alpha:1)
+        self.title = "Topics"
         
         self.navigationController?.navigationBar.tintColor = UIColor.whiteColor()
-        getTopics()
-        getEmotions()
+        topicPieChartView.delegate = self
         
+        
+        
+        let query = PFQuery(className:"DailyAverage")
+        query.orderByDescending("createdAt")
+        query.getFirstObjectInBackgroundWithBlock(){
+            (dailyAverage: PFObject?, error: NSError?) -> Void in
+            if let dailyAvg = dailyAverage, topicSamples = dailyAvg["topicSamples"] as? [String]
+                where error == nil {
+                self.topicTweetSamples = topicSamples
+            } else {
+                print(error)
+            }
+        }
+
+        getTopics()
     }
     
     func setTopicChart(dataPoints: [String], values: [Double]) {
@@ -76,6 +92,7 @@ class PieChartViewController: UIViewController {
         }
        
         pieChartDataSet.colors = colors
+        topicColors = colors
         
         topicPieChartView.legend.textColor = UIColor.whiteColor()
         topicPieChartView.descriptionTextColor = UIColor.whiteColor()
@@ -84,64 +101,23 @@ class PieChartViewController: UIViewController {
         topicPieChartView.legend.neededHeight = 20.0
         topicPieChartView.backgroundColor = UIColor.darkGrayColor()
         
+        
     }
     
-    func setEmotionChart(dataPoints: [String], values: [Double]) {
-        
-        var dataEntries: [ChartDataEntry] = []
-        
-        for i in 0..<dataPoints.count {
-            let dataEntry = ChartDataEntry(value: values[i], xIndex: i)
-            dataEntries.append(dataEntry)
-        }
-        let pieChartDataSet = PieChartDataSet(yVals: dataEntries, label: "")
-        let pieChartData = PieChartData(xVals: dataPoints, dataSet: pieChartDataSet)
-        emotionPieChartView.data = pieChartData
-        emotionPieChartView.centerText = "How is NU feeling?"
-        emotionPieChartView.centerTextLineBreakMode = NSLineBreakMode.ByWordWrapping
-        emotionPieChartView.descriptionText = "Emotions of NU Posts"
-        emotionPieChartView.usePercentValuesEnabled = true
-        emotionPieChartView.drawSliceTextEnabled = false
-        emotionPieChartView.descriptionTextColor = UIColor.whiteColor()
-        emotionPieChartView.legend.textColor = UIColor.whiteColor()
-        emotionPieChartView.backgroundColor = UIColor.darkGrayColor()
-        
-        var colors: [UIColor] = []
-        
-        for _ in 0..<dataPoints.count {
-            let red = Double(arc4random_uniform(256))
-            let green = Double(arc4random_uniform(256))
-            let blue = Double(arc4random_uniform(256))
-            colors.append(UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1))
-        }
-        
-        pieChartDataSet.colors = colors
-        
-        emotionPieChartView.legend.textColor = UIColor.whiteColor()
-        
-        emotionPieChartView.legendRenderer.computeLegend(pieChartData)
-        emotionPieChartView.legend.calculatedLabelBreakPoints = [false, false, false, false, true, false, false]
-        
-
-        emotionPieChartView.legend.neededHeight = 20.0
-        
-    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        
-        // Dispose of any resources that can be recreated.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func chartValueSelected(chartView: ChartViewBase, entry: ChartDataEntry, dataSetIndex: Int, highlight: ChartHighlight) {
+        if entry.xIndex < topics.count - 1 {
+            sampleTweetTopicLabel.text = "Sample tweet about \(topics[entry.xIndex])"
+            sampleTweetTopicLabel.textColor = topicColors[entry.xIndex]
+            
+            sampleTweetBodyLabel.text = "@anonymous: \(topicTweetSamples[entry.xIndex])"
+        }
     }
-    */
-
+    
+    
 }
